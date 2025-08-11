4b0914f092ade4fe8dc50a88ff6efa9036fcb743
Fredrik Wictorsson <wictorsson@gmail.com>
2025-08-11 23:41:44 +0200

    update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- Keep your existing settings
-config.color_scheme = "Gruvbox Material (Gogh)"
+config.color_scheme = "terafox"
 config.font = wezterm.font("MesloLGS nf")
 config.font_size = 16
 -- config.enable_tab_bar = false
@@ -12,13 +12,12 @@ config.window_decorations = "RESIZE"
 config.colors = {
 	tab_bar = {
 		active_tab = {
-			bg_color = "#282828",
-			-- fg_color = "#ebdbb2",
-			fg_color = "#a89984",
+			bg_color = "#152528",
+			fg_color = "#7AA4A1",
 		},
 		inactive_tab = {
-			bg_color = "#3c3836",
-			fg_color = "#1d2021",
+			bg_color = "#2F3239",
+			fg_color = "#73A3B7",
 		},
 	},
 }
