232356ba45dcb4890fc87636b4b242aae734bee5
Fredrik Wictorsson <wictorsson@gmail.com>
2026-03-10 16:13:14 +0100

    update sketchybar design

--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$SELECTED" = "true" ]; then
-  sketchybar --set "$NAME" icon.color=0xffffffff
+  sketchybar --set "$NAME" icon.color=0xff7f9e7f
 else
   sketchybar --set "$NAME" icon.color=0x1fffffff
 fi
